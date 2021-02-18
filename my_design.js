daee5226900a6230a1a1bbe647764abadd84b540
aith <ariiramanesh@gmail.com>
2021-02-18 14:50:47 -0800

    starter code '

--- my_design.js
+++ my_design.js
@@ -2,13 +2,22 @@
 
 
 function p4_inspirations() {
-  return [];
+  let imgs = [
+      {
+          name: "Nighthawks - Edward Hopper",
+          assetUrl: "https://cdn.glitch.com/a9ccced7-5149-4223-becd-92be9a575a62%2Fnighthawks.jpg?v=1613688224211"
+     }
+
+             ];
+  return imgs;
 }
 
 function p4_initialize(inspiration) {
   return {};
 }
 
-function p4_render(design, inspiration) {}
+function p4_render(design, inspiration) {
+}
 
-function p4_mutate(design, inspiration, rate) {}
+function p4_mutate(design, inspiration, rate) {
+}
