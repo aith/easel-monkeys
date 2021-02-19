992edefb24874b79573fd29732c67cdc29cab0e8
aith <ariiramanesh@gmail.com>
2021-02-18 16:07:06 -0800

    *Finished random Rect thing

--- my_design.js
+++ my_design.js
@@ -35,36 +35,32 @@ function draw_vertices(vertices) {
 }
 
 class Rect {
-    constructor(scale_x, scale_y, n_vertices, s_vertices, e_vertices, w_vertices) {
+    // NOTE the vertices order
+    constructor(scale_x, scale_y, n_vertices, e_vertices, s_vertices, w_vertices) {
         this.scale_x = scale_x
         this.scale_y = scale_y
         this.n_vertices = n_vertices
-        this.s_vertices = s_vertices
         this.e_vertices = e_vertices
+        this.s_vertices = s_vertices
         this.w_vertices = w_vertices
     }
-    // all_vertices() {
-    //     return this.n_vertices.concat(this.s_vertices, this.e_vertices, this.w_vertices);
-    // }
+    all_vertices() {
+        print(this.n_vertices.concat(this.e_vertices, this.s_vertices, this.w_vertices))
+        return this.n_vertices.concat(this.e_vertices, this.s_vertices, this.w_vertices);
+    }
 }
 
 
-let nv = [[20, 100], [300, 100], [500, 100]]
-let sv = [[20, 200], [300, 200], [500, 200]]
-let ev = [[500, 100], [500, 150], [500, 200]]
-let wv = [[20, 100], [20, 150], [20, 200]]
+let nv = [[20, 100],  [500, 100]]
+let ev = [[500, 100], [500, 200]]
+let sv = [[500, 200],  [20, 200]]
+let wv = [[20, 200],  [20, 100]]
 function p4_render(design, inspiration) {
     background("white")
     fill("blue")
-    ellipse(10,200,10,20)
-    nv = subdivide_edge(nv, 4, split_horizontal_edge)
-    draw_vertices(nv)
-    sv = subdivide_edge(sv, 4, split_horizontal_edge)
-    draw_vertices(sv)
-    wv = subdivide_edge(wv, 4, split_vertical_edge)
-    draw_vertices(wv)
-    ev = subdivide_edge(ev, 4, split_vertical_edge)
-    draw_vertices(ev)
+    let r = new Rect(1, 1, nv, ev, sv, wv)
+    subdivide_rect(r, 4);
+    draw_vertices(r.all_vertices())
 }
 
 function subdivide_edge(vertices, amt, split_edge_func) {
@@ -74,23 +70,20 @@ function subdivide_edge(vertices, amt, split_edge_func) {
         if(result.length > 0) {
             let additions = split_edge_func(result[0], result[1]);
             additions.forEach(v => { new_vertices.push(v) })
-            print(new_vertices)
         };
         for(let idx = 1; idx < result.length-1; idx++) {
-            print("inside")
             let additions = split_edge_func(result[idx], result[idx+1]);
             new_vertices.push(additions[1], additions[2])
         }
         result = new_vertices;
     }
-    print(result)
     return result;
 }
 
 function subdivide_rect(rect, amt) {
     rect.n_vertices = subdivide_edge(rect.n_vertices, 4, split_horizontal_edge)
+    rect.e_vertices = subdivide_edge(rect.e_vertices, 4, split_vertical_edge)
     rect.s_vertices = subdivide_edge(rect.s_vertices, 4, split_horizontal_edge)
-    rect.e_vertices = subdivide_edge(rect.w_vertices, 4, split_vertical_edge)
     rect.w_vertices = subdivide_edge(rect.w_vertices, 4, split_vertical_edge)
 }
 
